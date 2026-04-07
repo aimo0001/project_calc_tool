9f831ce758dc95777f4250b9c58a23e321eb9b2e
aimo0001 <aimo0001@stud.ek.dk>
2026-04-07 15:12:10 +0200

    initial commit

---
+++ src/main/java/com/example/projectcalctool/ProjectCalcToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectcalctool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCalcToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCalcToolApplication.class, args);
+    }
+
+}
